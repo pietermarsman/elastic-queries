8caccf645c9c89678cc09b2e5fda057a9a45baab
Pieter Marsman <pieter.marsman@politie.nl>
2017-05-26 14:24:55 +0200

    Original commit push -u origin master

---
+++ es_composer.py
@@ -0,0 +1,134 @@
+from json_composer import JsonComposer
+
+
+class Source(JsonComposer):
+    def __init__(self, fields):
+        super().__init__(_source=fields)
+
+
+class Query(JsonComposer):
+
+    def __init__(self, *args, combine='must'):
+        query = {
+            'bool': {
+                combine: list(args)
+            }
+        }
+        super().__init__(query=query)
+
+
+class Sort(JsonComposer):
+
+    def __init__(self, **kwargs):
+        super().__init__(sort=dict(**kwargs))
+
+
+# Full text queries
+
+class Match(Query):
+
+    def __init__(self, field, value, **kwargs):
+        super().__init__({'match': {field: value}}, **kwargs)
+
+
+class MatchPhrase(Query):
+
+    def __init__(self, field, value, **kwargs):
+        super().__init__({'match_phrase': {field: value}}, **kwargs)
+
+
+class MatchPhrasePrefix(Query):
+
+    def __init__(self, field, value, **kwargs):
+        super().__init__({'match_phrase_prefix': {field: {'query': value}}}, **kwargs)
+
+
+class MultiMatch(Query):
+
+    def __init__(self, fields, value, **kwargs):
+        # todo add **kwargs besides query
+        super().__init__({'multi_match': {'query': value, 'fields': fields}}, **kwargs)
+
+
+class Common(Query):
+
+    def __init__(self, field, value, combine='must'):
+        # todo add **kwargs besides query
+        super().__init__({'common': {field: {'query': value}}})
+
+
+class QueryString(Query):
+
+    def __init__(self, value):
+        # todo add **kwargs besides query
+        super().__init__({'query_string': {'query': value}})
+
+
+class SimpleQueryString(Query):
+
+    def __init__(self, value):
+        # todo add **kwargs besides query
+        super().__init__({'simple_query_string': {'query': value}})
+
+
+# Term level queries
+
+class Term(Query):
+
+    def __init__(self, field, value, **kwargs):
+        super().__init__({'term': {field: value}}, **kwargs)
+
+
+class Terms(Term):
+    pass
+
+
+class Range(Query):
+
+    def __init__(self, field, ranges, **kwargs):
+        super().__init__({'range': {field: ranges}}, **kwargs)
+
+
+class Exists(Query):
+    def __init__(self, field, **kwargs):
+        super().__init__({'exists': {'field': field}}, **kwargs)
+
+
+class Prefix(Query):
+
+    def __init__(self, field, value, **kwargs):
+        super().__init__({'prefix': {field: value}}, **kwargs)
+
+
+class Wildcard(Query):
+
+    def __init__(self, field, value, **kwargs):
+        super().__init__({'wildcard': {field: value}}, **kwargs)
+
+
+class Regexp(Query):
+
+    def __init__(self, field, value, **kwargs):
+        super().__init__({'regexp': {field: value}}, **kwargs)
+
+
+class Fuzzy(Query):
+
+    def __init__(self, field, value, **kwargs):
+        super().__init__({'fuzzy': {field: value}}, **kwargs)
+
+
+class Type(Query):
+
+    def __init__(self, type, **kwargs):
+        super().__init__({'type': {'value': type}}, **kwargs)
+
+
+class Ids(Query):
+
+    def __init__(self, ids, **kwargs):
+        super().__init__({'ids': {'values': ids}}, **kwargs)
+
+if __name__ == '__main__':
+    a = Source('text_clean') + Exists('text_clean') + Term(user=123)
+    print(a)
---
+++ json_composer.py
@@ -0,0 +1,43 @@
+import json
+
+
+class JsonComposer(dict):
+
+    def __add__(self, other):
+        # merge keys
+        for key in other:
+            if key in self:
+                # recursive merge of keys
+                self[key] = JsonComposer.combine_values(self[key], other[key])
+            else:
+                # simple merge: add key to self
+                self[key] = other[key]
+        return self
+
+    def __repr__(self):
+        return json.dumps(self, indent=4, sort_keys=True)
+
+    @staticmethod
+    def combine_values(value1, value2):
+        if type(value1) is dict and type(value2) is dict:
+            # merge dicts
+            return JsonComposer(value1) + JsonComposer(value2)
+
+        elif type(value1) is list and type(value2) is list:
+            # merge lists
+            return value1 + value2
+
+        else:
+            raise ValueError('Trying to combine %s with %s but I do not know how to do this.' % (type(value1), type(value2)))
+
+
+if __name__ == '__main__':
+    exclusive1 = JsonComposer({'a': 1})
+    exclusive2 = JsonComposer({'b': 2})
+    recursive1 = JsonComposer({'c': {'d': 3}})
+    recursive2 = JsonComposer({'c': {'e': 4}})
+    list1 = JsonComposer({'f': [5, 6, 7]})
+    list2 = JsonComposer({'f': [8, 9, 10]})
+
+
+    print(exclusive1 + exclusive2 + recursive1 + recursive2 + list1 + list2)
